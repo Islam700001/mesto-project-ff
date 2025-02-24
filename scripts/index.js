84b9fcf5d017cf462a3489d34004804c721320a0
Islam Kairkhanov <kairkhanoovis@yandex.kz>
2025-02-24 19:30:11 +0500

    project

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+const cardsContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
 
-// @todo: DOM узлы
+function handleDeleteCard(cardElement) {
+  cardElement.remove();
+}
 
-// @todo: Функция создания карточки
+function createCard({ name, link }, handleDelete) {
+  const cardElement = cardTemplate.cloneNode(true);
 
-// @todo: Функция удаления карточки
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Вывести карточки на страницу
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardTitle.textContent = name;
+
+  deleteButton.addEventListener('click', () => handleDelete(cardElement));
+
+  return cardElement;
+}
+
+initialCards.forEach((cardData) => {
+  const newCard = createCard(cardData, handleDeleteCard);
+  cardsContainer.append(newCard);
+});
